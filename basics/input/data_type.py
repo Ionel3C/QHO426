01173f53545541a9596424aee6b628c63c48aaaa
Ionel3C <>
2022-03-03 19:38:50 +0000

    T

---
+++ basics/input/data_type.py
@@ -0,0 +1,8 @@
+print("What is your name human?")
+name = input()
+print("How old are you(in years)?")
+age = input()
+print("How tall are you(in m)?")
+height = float(input())
+print("How much do you weight(in kg)?")
+weight = float(input())
