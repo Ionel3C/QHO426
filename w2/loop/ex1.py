ce4d73acb74d49f75b2cd8ad988f1f15360fbddb
Ionel3C <>
2022-03-05 10:15:19 +0000

    ex6

---
+++ w2/loop/ex1.py
@@ -0,0 +1,5 @@
+count = 1
+# condition: loop untill 10
+while count < 10:
+    print(count)
+    count = count + 1
---
+++ w2/loop/ex6.py
@@ -0,0 +1,7 @@
+n = int(input("Please enter a number:   "))
+while n > 0:
+  if n % 2 == 0:
+    print(n, "is a even number")
+  else:
+    print(n, "is odd number")
+  n = n - 1
