6d42e2c2193e9ddd5a4f15da30f09bf5f7141eef
Ionel3C <>
2022-03-09 22:00:37 +0000

    simple

---
+++ basics/repetitions/while_loop/simple.py
@@ -0,0 +1,11 @@
+i = 0
+while (i):
+  i = int(input("How many cables should I remove ?   "))
+  if i:
+    i = 1:
+  print("Removed cable.")
+    i >= 2:
+  print("Removed cable.", "\nRemoved cable.")
+    i >= 3:
+  print("Removed cable.", "\nRemoved cable.", "\nRemoved cable.")
+else  
