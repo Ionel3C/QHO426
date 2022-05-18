08bb5ca58f9491062e169b5055533f09cd6bd1d5
Ionel3C <>
2022-05-18 21:32:04 +0000

    all

---
+++ demos/w7_recap.py
@@ -0,0 +1,28 @@
+import csv
+import matplotlib.pyplot as plt
+
+def gather_data(n = 1):
+    with open("eve_data.csv", "w") as file:
+        csv_writer = csv.writer(file)
+        for i in range(n):
+            h = float(input("Enter height: "))
+            w = float(input("Enter weight: "))
+            csv_writer.writerow([h, w])
+
+def retrieve_data():
+    with open("eve_data.csv") as file:
+        heights = []
+        weights = []
+        csv_reader = csv.reader(file)
+        for row in csv_reader:
+            heights.append(float(row[0]))
+            weights.append(float(row[1]))
+    return heights, weights
+
+def graphs():
+    x, y = retrieve_data()
+    plt.plot(x, y, "go")
+    plt.show()
+
+
+graphs()
